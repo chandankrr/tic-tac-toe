ce68bff609dfe47ec146cbdafea00f778a5631ea
Chandan Kumar <chandankrr.91@gmail.com>
2023-01-14 19:29:26 +0530

    added sass, get to know styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,9 @@
 const Square = ({ value }) => {
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 };
 
 export default Square;
